4171fec3931247737dedf12735071cf63d1aef34
Khasan <mr.khasiev@list.ru>
2024-08-20 14:42:44 +0300

    vue query

---
+++ components/kanban/useKanbanQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/vue-query";
+import { COLLECTION_DEALS, DB_ID } from "~/lib/app.constants";
+import { DB } from "~/lib/appwrite";
+import { KANBAN_DATA } from "./kanban.data";
+import type { IDeal } from "~/types/deals.type";
+
+export function useKanbanQuery() {
+   return useQuery({
+      queryKey: ['deals'],
+      queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+      select(data) {
+         const newBoard = [...KANBAN_DATA]
+         const deals = data.documents as unknown as IDeal[]
+      },
+      
+   })
+}
