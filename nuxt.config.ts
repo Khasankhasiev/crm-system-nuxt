0e90ceb9b7dadec59297e97383524689b9782c5c
Khasan <mr.khasiev@list.ru>
2024-08-22 19:25:43 +0300

    создана форма для регистраци сделки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
-  modules: ["@nuxt/image", '@nuxtjs/tailwindcss', "shadcn-nuxt", [
-      "@nuxtjs/google-fonts", {
-       families: {
-         Lato: {
-           wght: [300, 400, 700],
-           ital: [300]
-         }
-       }
-      }
-  ], "@nuxt/icon", '@pinia/nuxt',], 
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: 'Ui',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
-  },
-  pinia: {
-    storesDirs: ['./store/**']
-  }
-
-})
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: false },
+    modules: [
+        '@nuxt/image',
+        '@nuxtjs/tailwindcss',
+        'shadcn-nuxt',
+        [
+            '@nuxtjs/google-fonts',
+            {
+                families: {
+                    Lato: {
+                        wght: [300, 400, 700],
+                        ital: [300],
+                    },
+                },
+            },
+        ],
+        '@nuxt/icon',
+        '@pinia/nuxt',
+        [
+            '@vee-validate/nuxt',
+            {
+                // disable or enable auto imports
+                autoImports: true,
+            },
+        ],
+    ],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: 'Ui',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui',
+    },
+    pinia: {
+        storesDirs: ['./store/**'],
+    },
+});
--- pages/Index.vue
+++ pages/Index.vue
@@ -1,55 +1,68 @@
 <template>
-   <div class="p-10">
-      <h1 class="font-bold text-2xl mb-10">CRM System</h1>
-      <div v-if="isLoading">Loading...</div>
-      <div v-else>
-         <div class="grid grid-cols-5 gap-16">
-            <div v-for="(column, index) in data" :key="column.id">
-               <div class="rounded bg-slate-700 py-1 px-6 mb-2 text-center">
-                  {{ column.name }}
-               </div>
-               <div>
-                  <UiCard v-for="card in column.items" :key="card.id" class="mb-3" draggable="true">
-                     <UiCardHeader role="button">
+    <div class="p-10">
+        <h1 class="font-bold text-2xl mb-10">CRM System</h1>
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+            <div class="grid grid-cols-5 gap-16">
+                <div v-for="(column, index) in data" :key="column.id">
+                    <div
+                        class="rounded bg-slate-700 py-1 px-6 mb-2 text-center"
+                    >
+                        {{ column.name }}
+                    </div>
+                    <div>
+                        <KanbanCreateDeal
+                            :refetch="refetch"
+                            :status="column.id"
+                        />
+                        <UiCard
+                            v-for="card in column.items"
+                            :key="card.id"
+                            class="mb-3"
+                            draggable="true"
+                        >
+                            <UiCardHeader role="button">
+                                <UiCardTitle>
+                                    {{ card.name }}
+                                </UiCardTitle>
 
-                        <UiCardTitle>
-                           {{ card.name }}
-                        </UiCardTitle> 
+                                <UiCardDescription class="mt-2 block">
+                                    {{ convertCurrency(card.price) }}
+                                </UiCardDescription>
+                            </UiCardHeader>
 
-                        <UiCardDescription class="mt-2 block">
-                           {{ convertCurrency(card.price) }}
-                        </UiCardDescription>
+                            <UiCardContent class="text-xs">
+                                {{ card.companyName }}
+                            </UiCardContent>
 
-                     </UiCardHeader>
-
-                     <UiCardContent class="text-xs">
-                        {{card.companyName}}
-                     </UiCardContent>
-
-                     <UiCardFooter>
-                        {{ dayjs(card.$createdAt).format('DD MMMM YYYY')}}
-                     </UiCardFooter>
-                  </UiCard>
-               </div>
+                            <UiCardFooter>
+                                {{
+                                    dayjs(card.$createdAt).format(
+                                        'DD MMMM YYYY'
+                                    )
+                                }}
+                            </UiCardFooter>
+                        </UiCard>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
 import type { ICard, IColumn } from '~/components/kanban/kanban.types';
 import { useKanbanQuery } from '~/components/kanban/useKanbanQuery';
-import { convertCurrency } from '~/lib/convertCurrency.js'
+import { convertCurrency } from '~/lib/convertCurrency.js';
 import dayjs from 'dayjs';
 
 useSeoMeta({
-   title: 'Home | CRM System'
-})
+    title: 'Home | CRM System',
+});
 
-const dragCardRef = ref<ICard | null>(null)
-const sourceColumnRef = ref<IColumn | null>(null)
+const dragCardRef = ref<ICard | null>(null);
+const sourceColumnRef = ref<IColumn | null>(null);
 
-const { data, isLoading, refetch} = useKanbanQuery()
+const { data, isLoading, refetch } = useKanbanQuery();
 // useKanbanQuery()
 </script>
